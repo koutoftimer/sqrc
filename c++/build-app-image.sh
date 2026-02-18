61abcb286eeb627222b2e3c07afb1b0174522dca
Ruslan Kovtun <koutoftimer@gmail.com>
2026-02-18 15:38:00 +0200

    Makes AppImage buildable again
    * Bumps minimum cmake version to be usable on most modern distributions
    * Uses `curl` to download appimage tools instead of `wget`
    * Removes `/dev/shm` in favor of just `/tmp` because `chmod` doesn't
      work for some reason
    * Adds some error messages to specify failure point (helps for debugging)
    * Adds "Categories" that are now mandatory for .desktop files

--- c++/build-app-image.sh
+++ c++/build-app-image.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Provide path to qmake for Qt5
+export QMAKE=/usr/lib64/qt5/bin/qmake
+
+die() {
+    echo -e "\n\033[0;31m[ERROR]\033[0m $1" >&2
+    exit 1
+}
+
+BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" sqrc-build-XXXXXX) || die "Could not create temporary directory"
+PROJECT_ROOT=$(readlink -f $(dirname $0))
+
 load_tools() {
     readonly -A tools=(
         ['appimagetool-x86_64.AppImage']='https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage'
@@ -7,8 +18,14 @@ load_tools() {
         ['linuxdeploy-plugin-qt-x86_64.AppImage']='https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage'
     )
     for tool in ${!tools[@]}; do
-        [ ! -f "$tool" ] && wget "${tools[$tool]}"
-        [ ! -x "$tool" ] && chmod +x "${tools[$tool]}"
+        local tool_path="$PROJECT_ROOT/$tool"
+        local tool_url="${tools[$tool]}"
+        if [ ! -f "$tool_path" ]; then
+            curl --output "$tool_path" --location "$tool_url" || die "Failed to download $tool"
+        fi
+        if [ ! -x "$tool_path" ]; then
+            chmod +x "$tool_path" || die "Failed to make $tool executable"
+        fi
     done
 
     echo [ + ] Tools ready.
@@ -16,14 +33,8 @@ load_tools() {
 
 load_tools
 
-if [ "$CI" == "" ] && [ -d /dev/shm ]; then
-    TEMP_BASE=/dev/shm
-else
-    TEMP_BASE=/tmp
-fi
+TEMP_BASE=/tmp
 
-BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" sqrc-build-XXXXXX)
-PROJECT_ROOT=$(readlink -f $(dirname $0))
 export QML_SOURCES_PATHS="$PROJECT_ROOT"/src
 
 if [ -d $BUILD_DIR ]; then
@@ -33,11 +44,14 @@ fi
 
 pushd "$BUILD_DIR"
 
-cmake "$PROJECT_ROOT" -DCMAKE_INSTALL_PREFIX=/usr \
-  && make -j$(nproc) \
-  && make install DESTDIR=AppDir \
-  && $PROJECT_ROOT/linuxdeploy-x86_64.AppImage --appdir AppDir --plugin qt --output appimage \
-  && mv SQRC-x86_64.AppImage $PROJECT_ROOT
+cmake "$PROJECT_ROOT" -DCMAKE_INSTALL_PREFIX=/usr || die "CMake configuration failed"
+make -j$(nproc) || die "Build failed"
+make install DESTDIR=AppDir || die "Installation to AppDir failed"
+
+export NO_STRIP=1
+"$PROJECT_ROOT/linuxdeploy-x86_64.AppImage" --appdir AppDir --plugin qt --executable src/sqrc --output appimage || die "linuxdeploy failed"
+
+mv SQRC-x86_64.AppImage $PROJECT_ROOT || die "Can't retrieve final AppImage"
 
 popd
 rm -rf $BUILD_DIR
